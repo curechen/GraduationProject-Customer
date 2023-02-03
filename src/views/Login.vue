a8bf826e9f81b7de3b1b38fba47efc16ce339f21
curechen <981470148@qq.com>
2023-02-03 11:29:44 +0800

    feat: 修改登录页logo

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -11,7 +11,8 @@
 <template>
   <div class="login">
     <s-header :name="type == 'login' ? '登录' : '注册'" :back="'/home'"></s-header>
-    <img class="logo" src="https://s.yezgea02.com/1604045825972/newbee-mall-vue3-app-logo.png" alt="">
+    <!-- <img class="logo" src="https://s.yezgea02.com/1604045825972/newbee-mall-vue3-app-logo.png" alt=""> -->
+    <img class="logo" src="@/assets/mall-logo.png" alt="">
     <div v-if="type == 'login'" class="login-body login">
       <van-form @submit="onSubmit">
         <van-field
@@ -154,8 +155,8 @@ export default {
 <style lang="less">
   .login {
     .logo {
-      width: 120px;
-      height: 120px;
+      width: 150px;
+      height: 150px;
       display: block;
       margin: 80px auto 20px;
     }
